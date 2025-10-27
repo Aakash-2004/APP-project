38737c930191b35da336622b4258cc306a8735f6
Aakash-2004 <120889386+Aakash-2004@users.noreply.github.com>
2025-10-27 23:18:41 +0530

    Create data.sql

---
+++ model/repository/service/controller/controller/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO LOCATION (name, latitude, longitude, description) VALUES 
+('Main Gate', 12.9716, 77.5946, 'Entrance of the campus'),
+('Library', 12.9722, 77.5955, 'Main campus library'),
+('Canteen', 12.9731, 77.5962, 'Student canteen near block A'),
+('Computer Lab', 12.9738, 77.5970, 'CS department lab on 2nd floor');

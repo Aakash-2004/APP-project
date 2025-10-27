34eb20248959d4648727f6942134e28261e59c29
Aakash-2004 <120889386+Aakash-2004@users.noreply.github.com>
2025-10-27 23:16:19 +0530

    Create CampusNavigationBackendApplication.java

---
+++ CampusNavigationBackendApplication.java
@@ -0,0 +1,12 @@
+package com.campus.navigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusNavigationBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampusNavigationBackendApplication.class, args);
+        System.out.println("🚀 Campus Navigation Backend is running...");
+    }
+}

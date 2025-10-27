cb4c26013d94728e049eca0d60a80b27b118220f
Aakash-2004 <120889386+Aakash-2004@users.noreply.github.com>
2025-10-27 23:16:39 +0530

    Create Location.java

---
+++ model/Location.java
@@ -0,0 +1,22 @@
+package com.campus.navigation.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double latitude;
+    private double longitude;
+    private String description;
+}
